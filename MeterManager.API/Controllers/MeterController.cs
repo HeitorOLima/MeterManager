9e8aec09aa6850ec452a57188409415a3b712fad
Heitor Lima <ho_lima@outlook.com>
2022-11-20 12:56:41 -0300

    MeterController adjustments

--- MeterManager.API/Controllers/MeterController.cs
+++ MeterManager.API/Controllers/MeterController.cs
@@ -1,12 +1,12 @@
 ﻿using MeterManager.API.Interfaces;
 using MeterManager.API.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MeterManager.API.Controllers
 {
     [Route("api")]
-    public class MeterController : Controller
+    [ApiController]
+    public class MeterController : ControllerBase
     {
         private readonly IMeterService _meterService;
 
@@ -14,15 +14,9 @@ namespace MeterManager.API.Controllers
         {
             _meterService = meterService;
         }
-    
-        // GET: MeterController
-        [Route("teste")]
-        public ActionResult Teste()
-        {
-            return Ok("Funcionando!");
-        }
 
-        // GET: MeterController/Details/5
+        [HttpGet]
+        [Route("meters/recover")]
         public ActionResult GetAllMeters()
         {
             try
@@ -30,18 +24,35 @@ namespace MeterManager.API.Controllers
                 var meters = _meterService.GetAllAsync();
 
                 if(meters == null)
-                    return NotFound("No meter was found in database");
+                    return NotFound("No meter were found in the database");
 
                 return Ok(meters);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                throw;
+                return BadRequest("An error occurred while trying to get the list of meters from the database:" + ex.Message);
             }
         }
 
-        // GET: MeterController/Create
-        [HttpPost]
+        [HttpGet("meters/recover/{serialNumber}")]
+        public async Task<IActionResult> GetBySerialNumberAsync(string serialNumber)
+        {
+            try
+            {
+                var meterModel = await _meterService.GetBySerialNumberAsync(serialNumber);
+
+                if (meterModel == null)
+                    return NotFound("The meter sought was not found in the database.");
+
+                return Ok(meterModel);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"An error ocurred while trying to recover the meter {serialNumber}:" + ex.Message);
+            }
+        }
+
+        [HttpPost("meters/create/{meter}")]
         public async Task<IActionResult> Create(MeterModel meter)
         {
             try
@@ -49,17 +60,17 @@ namespace MeterManager.API.Controllers
                 var createdMeter = await _meterService.CreateAsync(meter);
 
                 if (createdMeter == null)
-                    return BadRequest("The meter already exists in Database");
+                    return BadRequest("The attempt to create the meter was canceled because it already exists in the database.");
 
                 return Ok(createdMeter);
             }
             catch(Exception ex)
             {
-                return BadRequest("An error ocurred while trying to create the meter. Error log:" + ex.Message);
+                return BadRequest($"An error ocurred while trying to create the meter {meter.SerialNumber}:" + ex.Message);
             }
         }
 
-        // GET: MeterController/Edit/5
+        [HttpPost("meters/update/{meter}")]
         public async Task<IActionResult> Update(MeterModel meter)
         {
             try
@@ -67,36 +78,35 @@ namespace MeterManager.API.Controllers
                 var updatedMeter = _meterService.UpdateAsync(meter);
 
                 if (updatedMeter == null)
-                    return NotFound("The meter was not found on our database");
+                    return NotFound("The meter being updated was not found in the database.");
 
                 return Ok(updatedMeter);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
-                throw;
+                return BadRequest($"An error ocurred while trying to update the meter {meter.SerialNumber}:" + ex.Message);
             }
         }
-        // GET: MeterController/Delete/5
+
+        [HttpDelete("meters/delete/{serialNumber}")]
         public async Task<IActionResult> Delete(string serialNumber)
         {
-            var meterToDelete = _meterService.GetMeterModelToDelete(serialNumber);
+            try
+            {
+                var meterToDelete = _meterService.GetMeterModelToDelete(serialNumber);
 
-            if (meterToDelete == null)
-                return NotFound("The meter was not found");
+                if (meterToDelete == null)
+                    return NotFound("The meter being deleted was not found in the database.");
 
-            await _meterService.DeleteAsync(serialNumber);
-            return NoContent();
+                await _meterService.DeleteAsync(serialNumber);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"An error ocurred while trying to delete the meter {serialNumber}:" + ex.Message);
+            }
         }
 
-        public async Task<IActionResult> GetBySerialNumberAsync(string serialNumber)
-        {
-            var meterModel = await _meterService.GetBySerialNumberAsync(serialNumber);
-
-            if (meterModel == null)
-                return NotFound("The meter was not found");
-
-            return Ok(meterModel);
-        }
+        
     }
 }
