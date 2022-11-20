b29f41b6872b5d465b24ade1948e2cc407867788
Heitor Lima <ho_lima@outlook.com>
2022-11-20 12:36:57 -0300

    Implementation of the MeterService and its interface

--- MeterManager.API/Interfaces/IMeterService.cs
+++ MeterManager.API/Interfaces/IMeterService.cs
@@ -5,6 +5,12 @@ using MeterManager.API.Models;
 namespace MeterManager.API.Interfaces
 {
     public interface IMeterService
-    { 
+    {
+        Task<MeterModel> CreateAsync(MeterModel model);
+        Task UpdateAsync(MeterModel model);
+        Task DeleteAsync(string serialNumber);
+        Task<List<MeterModel>> GetAllAsync();
+        Task<MeterModel> GetBySerialNumberAsync(string serialNumber);
+        Task<MeterModel> GetMeterModelToDelete(string serialNumber);
     }
 }
--- MeterManager.API/Services/MeterService.cs
+++ MeterManager.API/Services/MeterService.cs
@@ -1,9 +1,64 @@
 ﻿using MeterManager.API.Interfaces;
 using MeterManager.API.Models;
+using MeterManager.API.Repositories;
+using AutoMapper;
 
 namespace MeterManager.API.Services
 {
     public class MeterService : IMeterService
     {
+        private readonly IMeterRepository _meterRepository;
+        private readonly IMapper _mapper;
+        public MeterService(MeterRepository meterRepository, IMapper mapper)
+        {
+            _meterRepository = meterRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<MeterModel> CreateAsync(MeterModel model)
+        {
+            var serialNumberIsUnique = CheckIfMeterExists(model);
+
+            if (!serialNumberIsUnique)
+                return null;
+            
+            return await _meterRepository.CreateAsync(model);
+        }
+
+        public async Task DeleteAsync(string serialNumber)
+        {
+            await _meterRepository.DeleteAsync(serialNumber);
+        }
+
+        public Task<List<MeterModel>> GetAllAsync()
+        {
+            return _meterRepository.GetAllAsync();
+        }
+
+        public Task<MeterModel> GetBySerialNumberAsync(string serialNumber)
+        {
+            return _meterRepository.GetBySerialNumberAsync(serialNumber);
+        }
+
+        public Task UpdateAsync(MeterModel model)
+        {
+            var meterExist = CheckIfMeterExists(model);
+            if (!meterExist)
+                return null;
+                
+            return _meterRepository.UpdateAsync(model);
+        }
+
+        private bool CheckIfMeterExists(MeterModel model)
+        {
+            var isUnique = _meterRepository.GetBySerialNumberAsync(model.SerialNumber) == null ? true : false;
+            return isUnique;    
+        }
+
+        public Task<MeterModel> GetMeterModelToDelete(string serialNumber)
+        {
+            var meter = _meterRepository.GetBySerialNumberAsync(serialNumber);
+            return meter;
+        }
     }
 }
